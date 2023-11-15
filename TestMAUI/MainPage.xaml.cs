7301c550de4114a86ac5053e56e47380b22a4a20
Oliver Spencer <email@oliverspencer.com>
2023-11-15 12:45:29 +0000

    Add project files.

---
+++ TestMAUI/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace TestMAUI;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+
+        IList<string> cities = null;     //This will generate a NullReferenceException....
+        foreach (var city in cities)
+        {
+            Console.WriteLine(city);
+        }
+    }
+}
+
